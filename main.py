709399f37b1dfcde2f038d0d717b46f5aa1c002f
Arseney Abraamyan <ar.abraamyan@gmail.com>
2020-08-26 17:06:15 +0300

    add two urls

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_socketio import SocketIO
 from flask_restful import Resource, Api
 from flask_migrate import Migrate
 from flask_migrate import MigrateCommand
 from flask_script import Manager
 
 from config import Configuration
+from resources.alarm_clock import AlarmClock
+from resources.alarm_clock import AlarmClockList 
+
+from db import db
 
 
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
-db = SQLAlchemy()
+api = Api(app)
 db.init_app(app)
 migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
-api = Api(app)
+socketio = SocketIO(app)
+
+api.add_resource(AlarmClock, "/alarm_clock")
+api.add_resource(AlarmClockList, "/list_alarm_clock")
+
 
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app)
--- models.py
+++ models.py
@@ -1,4 +1,6 @@
-from main import db
+from db import db
+from datetime import datetime
+
 
 
 class AlarmClockModel(db.Model):
@@ -6,8 +8,23 @@ class AlarmClockModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     ring_time = db.Column(db.DateTime, nullable=False)
-    worked = db.Column(db.Boolean, default=False, nullable=False)
+    worked = db.Column(db.Boolean, default=False)
     description = db.Column(db.Text)
 
     def __repr__(self):
         return "<Alarm Clock id: {}, ringing time: {}>".format(self.id, self.ring_time)
+
+
+    def __init__(self, ring_time, description):
+        self.ring_time = ring_time
+        self.description = description
+        self.worked = False
+
+
+    def json(self):
+        return {"ring_time": str(self.ring_time), "description" : self.description}
+
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
